386cc3bfcca559f1fd784458f60bb4430d4b386c
JeremyLleva <llevajeremy@yahoo.com>
2020-08-12 14:59:17 -0700

    Added Modal for adding entries

--- screens/MealsScreen.js
+++ screens/MealsScreen.js
@@ -1,18 +1,51 @@
 /** @format */
 
-import React from 'react'
+import React, { useState } from 'react'
 import {
     View,
     Text,
     FlatList,
     StyleSheet,
     TouchableOpacity,
+    Button,
+    Modal,
 } from 'react-native'
+import DateTimePicker from '@react-native-community/datetimepicker'
 
 const MealsScreen = (props) => {
+    const [date, setDate] = useState(new Date())
+    const [addLogVisibility, setAddLogVisibility] = useState(false)
     return (
         <View style={styles.screen}>
             <Text> This is the Meals Screen</Text>
+            <Button
+                title='Add Entry'
+                onPress={() => {
+                    setAddLogVisibility(true)
+                }}
+            />
+            <Modal animationType='slide' visible={addLogVisibility}>
+                <View style={styles.modalContainer}>
+                    <Text>Create a entry!</Text>
+                    <View style={styles.modalButtons}>
+                        <Button
+                            title='Add'
+                            onPress={() => {
+                                setAddLogVisibility(false)
+                            }}
+                        />
+                    </View>
+                    <View style={styles.modalButtons}>
+                        <Button
+                            title='Cancel'
+                            onPress={() => {
+                                setAddLogVisibility(false)
+                            }}
+                        />
+                    </View>
+                    <DateTimePicker value={date} />
+                </View>
+            </Modal>
         </View>
     )
 }
@@ -22,6 +55,15 @@ const styles = StyleSheet.create({
         backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center',
+    },
+    modalContainer: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    modalButtons: {
+        padding: 10,
     },
 })
 export default MealsScreen
